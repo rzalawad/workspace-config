6e7a3a64b50f90749510ace8523d46a50255254c
rzalawad <zalavadiaridham@gmail.com>
2023-11-10 12:46:46 -0800

    config for debugger navigation

--- config/nvim/lua/debugging.lua
+++ config/nvim/lua/debugging.lua
@@ -7,3 +7,5 @@ vim.api.nvim_set_keymap("n", "<Leader>dt", ":lua require('dapui').toggle()<CR>",
 vim.api.nvim_set_keymap("n", "<Leader>db", ":DapToggleBreakpoint<CR>", { noremap = true})
 vim.api.nvim_set_keymap("n", "<Leader>dc", ":DapContinue<CR>", { noremap = true})
 vim.api.nvim_set_keymap("n", "<Leader>dr", ":lua require('dapui').open({reset = true})<CR>", { noremap = true})
+vim.api.nvim_set_keymap("n", "<Leader>ds", ":DapStepOver<CR>", { noremap = true})
+vim.api.nvim_set_keymap("n", "<Leader>di", ":DapStepInto<CR>", { noremap = true})
