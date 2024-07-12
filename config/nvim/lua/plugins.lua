9688226cfd81ed74ad5d49236314f27a65ad4161
rzalawad <zalavadiaridham@gmail.com>
2024-07-12 12:37:56 -0700

    remove debug mode for copilot

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -106,10 +106,7 @@ plugins = {
         dependencies = {
             { "zbirenbaum/copilot.lua" },
             { "nvim-lua/plenary.nvim" },
-        },
-        opts = {
-            debug = true,
-        },
+        }
     },
 }
 
