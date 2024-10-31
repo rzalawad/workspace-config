47691ce659cbd7fb3f4d90c7c23ef8955faa93ba
Ridham Zalawadia <zalavadiaridham@gmail.com>
2024-10-30 22:14:27 -0700

    feat(nvim): add octo to review pr from neovim

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -173,6 +173,17 @@ plugins = {
                 ft = { "markdown", "Avante" },
             },
         },
+    },
+    {
+        'pwntester/octo.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope.nvim',
+            'nvim-tree/nvim-web-devicons',
+        },
+        config = function()
+            require "octo".setup()
+        end
     }
 }
 
