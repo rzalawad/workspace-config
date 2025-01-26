8655829e9579b06a31e70a9dcf5ad125e8a486f8
Ridham Zalawadia <zalavadiaridham@gmail.com>
2025-01-26 00:19:24 -0800

    fix(skhd): applescript copy bug where text would be copied only sometimes. issues was to increase delay since copy is a gui action

--- dot_config/scripts/executable_append_copy_mac.sh
+++ dot_config/scripts/executable_append_copy_mac.sh
@@ -2,11 +2,25 @@
 
 osascript -e '
 tell application "System Events"
+    set the_app to name of the first application process whose frontmost is true
+end tell
+
+
+tell application the_app
+    activate
+end tell
+
+tell application "System Events"
     set currentClipboard to (get the clipboard)
     keystroke "c" using command down
-    delay 0.3
+    delay 1
     set selectedText to (get the clipboard)
+
+    set the_focus_app to the_app
+
+    # display dialog the_app & ":" & selectedText
 
     set the clipboard to (currentClipboard & " " & selectedText)
 end tell
 '
+
